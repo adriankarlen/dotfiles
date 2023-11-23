5d7042b8d4a14269fb2f2a53df9b486cb0dbdf83
Adrian Karlén <113891532+adriankarlen@users.noreply.github.com>
2023-11-23 13:24:27 +0100

    fix: add Confirm-Elevated function

--- setup.ps1
+++ setup.ps1
@@ -2,8 +2,17 @@
 #                              installation script                             #
 # ---------------------------------------------------------------------------- #
 
+# ----------------------------- helper functions ----------------------------- #
+function Confirm-Elevated {
+    # Get the ID and security principal of the current user account
+    $myIdentity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $myPrincipal = new-object System.Security.Principal.WindowsPrincipal($myIdentity)
+    # Check to see if we are currently running "as Administrator"
+    return $myPrincipal.IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
+}
+
 # -------- check to see if we are currently running "as Administrator" ------- #
-if (!(Verify-Elevated)) {
+if (!(Confirm-Elevated)) {
     $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
     $newProcess.Arguments = $myInvocation.MyCommand.Definition;
     $newProcess.Verb = "runas";
