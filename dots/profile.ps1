7f565f35bdee5a6352fc510d8bf41fdede160c98
Adrian Karlén <adrian.karlen@spp.se>
2024-12-06 15:54:03 +0100

    chore: update pwsh profile

--- dots/profile.ps1
+++ dots/profile.ps1
@@ -1,11 +1,21 @@
-# ---------------------------------------------------------------------------- #
-#                                my pwsh profile                               #
-# ---------------------------------------------------------------------------- #
+# ─[ my pwsh profile ]────────────────────────────────────────────────────
 
-# ------------------------------ starship setup ------------------------------ #
+# ─[ prompt ]─────────────────────────────────────────────────────────────
 Invoke-Expression (&starship init powershell)
 
-# ------------------------------ genaral aliases ----------------------------- #
+prompt = ""
+function Invoke-Starship-PreCommand {
+    $current_location = $executionContext.SessionState.Path.CurrentLocation
+    if ($current_location.Provider.Name -eq "FileSystem") {
+        $ansi_escape = [char]27
+        $provider_path = $current_location.ProviderPath -replace "\\", "/"
+        $prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
+    }
+    $host.ui.Write($prompt)
+}
+
+
+# ─[ genaral aliases ]────────────────────────────────────────────────────
 function touch { Set-Content -Path ($args[0]) -Value ($null) } 
 function reload { . $PROFILE }
 function x { exit }
@@ -28,81 +38,75 @@ function unzip {
 Register-ArgumentCompleter -CommandName 'touch' -ParameterName args -ScriptBlock ${function:TabComplete}
 Register-ArgumentCompleter -CommandName 'unzip'  -ParameterName args -ScriptBlock ${function:TabComplete}
 
-# -------------------------------- git aliases ------------------------------- #
+# ─[ git aliases ]────────────────────────────────────────────────────────
 function add {
     git add $args 
-    if ($args -eq $null) { git add . } 
+    if ($args -eq $null) { git add . }
 }
+function clone { git clone $args }
+function checkout { git checkout $args }
 function commit { git commit -m $args }
 function push { git push }
-function pull { git pull }
+function pull { git fetch && git pull }
 function stat { git status }
 function gdiff { git diff HEAD }
 function vdiff { git difftool HEAD }
 function log { git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit }
 function lg { lazygit }
 
-# --------------------------------- komorebi --------------------------------- #
-$env:KOMOREBI_CONFIG_HOME = "$env:USERPROFILE\.config\komorebi"
-function komostart { komorebic start -c "$env:USERPROFILE\.config\komorebi\config.json" --whkd }
-function komo2mstart { komorebic start -c "$env:USERPROFILE\.config\komorebi\config-2M.json" --whkd }
 
-# ----------------------------------- whkd ----------------------------------- #
-function whkd-restart { taskkill /f /im whkd.exe && Start-Process whkd -WindowStyle hidden }
-
-# ---------------------------------- glazewm --------------------------------- #
-function glaze { glazewm --config="$env:USERPROFILE\.config\glazewm\config.yaml" }
-
-# _-------------------------------- fzf setup -------------------------------- #
-$ENV:FZF_DEFAULT_OPTS=@"
-	--color=fg:#908caa,bg:#191724,hl:#ebbcba
-	--color=fg+:#e0def4,bg+:#26233a,hl+:#ebbcba
-	--color=border:#403d52,header:#31748f,gutter:#191724
-	--color=spinner:#f6c177,info:#9ccfd8,separator:#403d52
-	--color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa
+# ─[ fzf setup ]──────────────────────────────────────────────────────────
+$ENV:FZF_DEFAULT_OPTS = @"
+    --color=fg:#908caa,bg:#232136,hl:#ea9a97
+    --color=fg+:#e0def4,bg+:#393552,hl+:#ea9a97
+    --color=border:#44415a,header:#3e8fb0,gutter:#232136
+    --color=spinner:#f6c177,info:#9ccfd8
+    --color=pointer:#c4a7e7,marker:#eb6f92,prompt:#908caa
 "@
 
-# ---------------------------- faster code launch ---------------------------- #
+# ─[ faster code launch ]─────────────────────────────────────────────────
 function c { code . }
-function n {
-  wezterm cli set-tab-title nvim;
-  nvim $args
+function vim { nvim $args }
+function n { nvim $args }
+
+# ─[ faster movement ]────────────────────────────────────────────────────
+Remove-Item alias:cd -Force
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+function cd {
+    param (
+        [Parameter(Mandatory = $false, Position = 0)]
+        [string]$args
+    )
+
+    if ($args -eq "") { z } else { z $args }
+
+    $curr_dir = GetCurrentDir(Get-Location)
+    $title = " " + $curr_dir
+    wezterm cli set-tab-title $title;
 }
 
-# ------------------------------ faster movement ----------------------------- #
-function .. { z ../$args }
-function ..2 { z ../../$args }
-function ..3 { z ../../../$args }
-function ..4 { z ../../../../$args }
-function ..5 { z ../../../../../$args }
-function gor { z /$args }
-function goc { z ~/.config/$args }
-function gon { z ~/AppData/Local/nvim }
-function godev { z ~/source/repos/$args }
+function .. { cd ../$args }
+function ..2 { cd ../../$args }
+function ..3 { cd ../../../$args }
+function ..4 { cd ../../../../$args }
+function ..5 { cd ../../../../../$args }
+function gor { cd /$args }
+function goc { cd ~/.config/$args }
+function gon { cd ~/AppData/Local/nvim }
+function godev { cd ~/source/repos/$args }
 
-# Register the tab completion function for the movement commands
-# Register-ArgumentCompleter -CommandName '..'  -ParameterName args -ScriptBlock ${function:TabComplete}
-# Register-ArgumentCompleter -CommandName '..2' -ParameterName args -ScriptBlock ${function:TabComplete}
-# Register-ArgumentCompleter -CommandName '..3' -ParameterName args -ScriptBlock ${function:TabComplete}
-# Register-ArgumentCompleter -CommandName '..4' -ParameterName args -ScriptBlock ${function:TabComplete}
-# Register-ArgumentCompleter -CommandName '..5' -ParameterName args -ScriptBlock ${function:TabComplete}
-# Register-ArgumentCompleter -CommandName 'gor'  -ParameterName args -ScriptBlock ${function:TabComplete}
-# Register-ArgumentCompleter -CommandName 'goh'  -ParameterName args -ScriptBlock ${function:TabComplete}
-# Register-ArgumentCompleter -CommandName 'goc'  -ParameterName args -ScriptBlock ${function:TabComplete}
-# Register-ArgumentCompleter -CommandName 'godev'  -ParameterName args -ScriptBlock ${function:TabComplete}
+─[ even faster movement ]───────────────────────────────────────────────
+function t { cd (Get-Item $(fzf --query=$args)).Directory.FullName }
 
-# --------------------------- even faster movement --------------------------- #
-function go { z (Get-Item $(fzf --query=$args)).Directory.FullName }
-
-# -------------------------------- eza aliases ------------------------------- #
+# ─[ eza aliases ]────────────────────────────────────────────────────────
 Remove-Item alias:ls -Force
 function ls { eza --icons=always --color=always --group-directories-first $args }
 function la { eza --icons=always --color=always --group-directories-first -a $args }
 function ll { eza --icons=always --color=always --group-directories-first -la --no-time $args }
 function lt { eza --icons=always --color=always --group-directories-first -T $args }
 
-# ------------------------------------ lf ------------------------------------ #
-
+# ─[ lf ]─────────────────────────────────────────────────────────────────
 Register-ArgumentCompleter -Native -CommandName 'lf' -ScriptBlock {
     param($wordToComplete)
     $completions = @(
@@ -128,8 +132,53 @@ Register-ArgumentCompleter -Native -CommandName 'lf' -ScriptBlock {
     }
 }
 
-# ----------------------------- helper functions ----------------------------- #
-# TODO: FIX THIS FUNCTION (IT ALWAYS RETURNS AS IF )
+# ─[ azure ]──────────────────────────────────────────────────────────────
+Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+    $completion_file = New-TemporaryFile
+    $env:ARGCOMPLETE_USE_TEMPFILES = 1
+    $env:_ARGCOMPLETE_STDOUT_FILENAME = $completion_file
+    $env:COMP_LINE = $wordToComplete
+    $env:COMP_POINT = $cursorPosition
+    $env:_ARGCOMPLETE = 1
+    $env:_ARGCOMPLETE_SUPPRESS_SPACE = 0
+    $env:_ARGCOMPLETE_IFS = "`n"
+    $env:_ARGCOMPLETE_SHELL = 'powershell'
+    az 2>&1 | Out-Null
+    Get-Content $completion_file | Sort-Object | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, "ParameterValue", $_)
+    }
+    Remove-Item $completion_file, Env:\_ARGCOMPLETE_STDOUT_FILENAME, Env:\ARGCOMPLETE_USE_TEMPFILES, Env:\COMP_LINE, Env:\COMP_POINT, Env:\_ARGCOMPLETE, Env:\_ARGCOMPLETE_SUPPRESS_SPACE, Env:\_ARGCOMPLETE_IFS, Env:\_ARGCOMPLETE_SHELL
+}
+
+ # ─[ roslyn nvim update ]─────────────────────────────────────────────────
+function roslyn() {
+  if ($args -eq "update") {
+    $file = New-Guid
+    Invoke-WebRequest https://github.com/Crashdummyy/roslynLanguageServer/releases/latest/download/microsoft.codeanalysis.languageserver.win-x64.zip -OutFile ~/Downloads/$file.zip
+    Expand-Archive ~/Downloads/$file.zip -DestinationPath ~/AppData/Local/nvim-data/roslyn/ -Force
+    rm ~/Downloads/$file.zip
+  }
+}
+
+# ─[ yazi ]───────────────────────────────────────────────────────────────
+function yy {
+    $tmp = [System.IO.Path]::GetTempFileName()
+    yazi $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp
+    if (-not [String]::IsNullOrEmpty($cwd) -and $cwd -ne $PWD.Path) {
+        Set-Location -LiteralPath $cwd
+    }
+    Remove-Item -Path $tmp
+}
+
+# ─[ symbolic link ]──────────────────────────────────────────────────────
+function make-link ($target, $link) {
+    New-Item -Path $link -ItemType SymbolicLink -Value $target
+}
+
+# ─[ helper functions ]───────────────────────────────────────────────────
+# TODO: FIX THIS FUNCTION (IT ALWAYS RETURNS AS IF $destination is pwd)
 function TabComplete {
     param (
         $commandName,
@@ -167,17 +216,18 @@ function TabComplete {
     }
 }
 
-prompt = ""
-function Invoke-Starship-PreCommand {
-    $current_location = $executionContext.SessionState.Path.CurrentLocation
-    if ($current_location.Provider.Name -eq "FileSystem") {
-        $ansi_escape = [char]27
-        $provider_path = $current_location.ProviderPath -replace "\\", "/"
-        $prompt = "$ansi_escape]7;file://${env:COMPUTERNAME}/${provider_path}$ansi_escape\"
+function GetCurrentDir {
+    param (
+      [string]$path = ""
+    )
+    if ($path -eq "") {
+      $path = Get-Location
     }
-    $host.ui.Write($prompt)
+
+    if ($path -eq "$env:USERPROFILE") {
+      return "~"
+    }
+
+    return Split-Path ($path) -Leaf 
 }
 
-Invoke-Expression (&starship init powershell)
-
-Invoke-Expression (& { (zoxide init powershell | Out-String) })
